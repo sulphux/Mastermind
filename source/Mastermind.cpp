b999b4ebed75a1b046369379f9bcdd47f1d05a0c
Bogdan Cichy <sulphux@gmail.com>
2022-06-13 17:08:16 +0200

    String tiding

--- source/Mastermind.cpp
+++ source/Mastermind.cpp
@@ -1,47 +1,54 @@
 ﻿#include <iostream>
-#include <vector>
 
 #include "GameSession.h"
-#include "CodeFactory.h"
-#include "Code.h"
-#include "Arbiter.h"
 
 
 int main()
 {
 	GameSession gameSession;
 
+	const std::string SEPARATOR = "---------------------------------";
+	const std::string WIN_MESSAGE = "CONGRATULATIONS!!YOU WIN!!!";
+	const std::string FAIL_MESSAGE = "Sorry too many attempts! Game finished!";
+	const std::string WELCOME_MESSAGE= "Welcome to Mastermind game!\n\n"
+									   "Try to guess the code! Code has 4 digits, which should be unique \n"
+									   "each other and also in range = [1,8]\n\n"
+									   "Let's start!";
+	const std::string AGAIN_MESSAGE = "Wanna play a new game ?";
+	const std::string BYE_MESSAGE = "See you later!";
+	
 
-	std::cout << "Welcome to Mastermind game!" << std::endl;
-	std::cout << std::endl;
-	std::cout << "Try to guess the code! Code has 4 digits, which should be unique \neach other and also in range = [1,8] " << std::endl;
+
+
+	std::cout << WELCOME_MESSAGE << std::endl;
 	std::cout << std::endl;
 
-	char s = '\0';
+	char userChoice = '\0';
 
 	do {
 		gameSession.startNewGame();
 		do {
-			std::cout << std::endl << "############################################################" << std::endl;
+			std::cout << std::endl << SEPARATOR<< std::endl;
 			gameSession.showPlayerGuesses();
 
 			std::string newCodeStr = gameSession.getGuessCodeFromPlayer();
 			gameSession.judgeNewCodeAndStore(newCodeStr);
 
 			if (gameSession.isPlayerWinner()) {
-				std::cout << "CONGRATULATIONS!! YOU WIN!!!" << std::endl;
+				std::cout << WIN_MESSAGE << std::endl;
 			}
 			else if (gameSession.isGameFinished()) {
-				std::cout << "Sorry too many attempts! Game finished!" << std::endl;
+				std::cout << std::endl << SEPARATOR << std::endl;
+				std::cout << FAIL_MESSAGE << std::endl;
 				gameSession.showPlayerGuesses();
 				gameSession.printSolution();
 			}
 		} while (!gameSession.isGameFinished());
 
-		std::cout << "Wanna play a new game?[y/N]" << std::endl;
+		std::cout << AGAIN_MESSAGE + "[y/N]" << std::endl;
 
-		std::cin >> s;
-	} while (s == 'y' || s == 'Y');
-	std::cout << "BYE!";
+		std::cin >> userChoice;
+	} while (userChoice == 'y' || userChoice == 'Y');
+	std::cout << BYE_MESSAGE << std::endl;
 	return 0;
 }
